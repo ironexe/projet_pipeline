cbf5ec7ace73d59e8f9d7d937f32f751df4129af
ironexe <itzmeachraaf227@email.com>
2026-04-23 14:58:08 +0100

    version ng

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 2 - Déployée "
+    return "Version 3 - Déployée "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
